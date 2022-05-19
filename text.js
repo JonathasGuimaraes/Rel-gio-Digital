61285112f6c3210bccc0805b53963ae7c90304de
JonathasGuimaraes <jonathasoliveiraguimaraes48@gmail.com>
2022-05-18 22:26:16 -0300

    Concluido o Relógio

---
+++ text.js
@@ -0,0 +1,18 @@
+const horas=document.getElementById('horas');
+const minutos=document.getElementById('minutos');
+const segundos=document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+          let today=new Date();
+           let h=today.getHours();
+           let m=today.getMinutes();
+           let s=today.getSeconds();
+    if (h<10)h = '0' + h;
+    if (m<10)m = '0' + m;
+    if (s<10)s = '0' + s;
+horas.textContent = h;
+minutos.textContent = m;
+segundos.textContent = s;
+
+}
+)
